1d213cf7a4fc35b1d36e7cd97d24f2d53fb63246
hbprotoss <gamespy1991@gmail.com>
2017-07-14 14:32:58 +0800

    typeMapping

--- generator/wsdl/typeMapping.go
+++ generator/wsdl/typeMapping.go
@@ -1,7 +1,22 @@
 package wsdl
 
 var typeMapping = map[string]string{
+	// string
 	"string":           "string",
 	"normalizedString": "string",
 	"token":            "string",
+	// date
+	"date":     "time",
+	"time":     "time",
+	"dateTime": "time",
+	// number
+	"byte":    "byte",
+	"short":   "int16",
+	"int":     "int32",
+	"integer": "int32",
+	"long":    "int64",
+	"decimal": "??",
+	// misc
+	"boolean": "bool",
+	"any":     "interface{}",
 }
